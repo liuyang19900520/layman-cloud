a524db6e0528aeb3168ae800a6aa98b168196b56
liuyang19900520 <liuyang19900520@hotmail.com>
2022-06-05 10:50:30 +0900

    nacos init

--- laymanmall-coupon/src/main/java/com/liuyang19900520/laymanmall/coupon/LaymanmallCouponApplication.java
+++ laymanmall-coupon/src/main/java/com/liuyang19900520/laymanmall/coupon/LaymanmallCouponApplication.java
@@ -2,8 +2,10 @@ package com.liuyang19900520.laymanmall.coupon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class LaymanmallCouponApplication {
 
   public static void main(String[] args) {
