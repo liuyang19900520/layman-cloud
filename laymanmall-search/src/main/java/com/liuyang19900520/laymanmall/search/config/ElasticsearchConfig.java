dc7bfec28b1efd98f78edb0939f81a769f738642
liuyang19900520 <liuyang19900520@hotmail.com>
2022-09-01 23:58:36 +0900

    set ware api

--- laymanmall-search/src/main/java/com/liuyang19900520/laymanmall/search/config/ElasticsearchConfig.java
+++ laymanmall-search/src/main/java/com/liuyang19900520/laymanmall/search/config/ElasticsearchConfig.java
@@ -1,5 +1,14 @@
 package com.liuyang19900520.laymanmall.search.config;
 
+import lombok.Data;
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
+
 /**
  * <p>
  *
@@ -8,10 +17,20 @@ package com.liuyang19900520.laymanmall.search.config;
  * @author Max Liu
  * @since 2022/08/13
  */
-//@ConfigurationProperties(prefix = "elasticsearch")
-//@Configuration
-//@Data
-//public class ElasticsearchConfig extends AbstractElasticsearchConfiguration {
-//
-//
-//}
+@ConfigurationProperties(prefix = "elasticsearch")
+@Configuration
+@Data
+public class ElasticsearchConfig extends AbstractElasticsearchConfiguration {
+
+  private String host;
+  private Integer port;
+
+  //重写父类方法
+  @Override
+
+  public RestHighLevelClient elasticsearchClient() {
+    RestClientBuilder builder = RestClient.builder(new HttpHost(host, port));
+    RestHighLevelClient restHighLevelClient = new RestHighLevelClient(builder);
+    return restHighLevelClient;
+  }
+}
